4e8d7c4385c02b0796ae9c104c01d63e64e4aa3e
William Sung <sungkinfngwilliam@gmail.com>
2013-02-02 02:22:58 -0600

    change application to app

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import flask
  
-application = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
-#Set application.debug=true to enable tracebacks on Beanstalk log output. 
+#Set app.debug=true to enable tracebacks on Beanstalk log output. 
 #Make sure to remove this line before deploying to production.
-application.debug=True
+app.debug=True
  
-@application.route('/')
+@app.route('/')
 def hello_world():
     return "Hello world!"
 
-@application.route('/<name>')
+@app.route('/<name>')
 def name(name):
     return "<h1>Hello %s,</h1><p>Welcome here!</p>" % name
  
-@application.route('/Dan')
+@app.route('/Dan')
 def Dan():
     return "<h2>Dan is a meat!! :)</h2>"
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
