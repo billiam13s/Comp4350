96e1da485d20628c793bda96d98306069b48a826
William Sung <sungkinfngwilliam@gmail.com>
2013-02-02 13:22:52 -0600

    Fix again

--- application.py
+++ application.py
@@ -1,22 +1,22 @@
 import flask
  
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
-#Set app.debug=true to enable tracebacks on Beanstalk log output. 
+#Set application.debug=true to enable tracebacks on Beanstalk log output. 
 #Make sure to remove this line before deploying to production.
-app.debug=True
+application.debug=True
  
-@app.route('/')
+@application.route('/')
 def hello_world():
     return "Hello world!"
 
-@app.route('/<name>')
+@application.route('/<name>')
 def name(name):
     return "<h1>Hello %s,</h1><p>Welcome here!</p>" % name
  
-@app.route('/Dan')
+@application.route('/Dan')
 def Dan():
     return "<h2>Dan is a meat!! :)</h2>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0', debug=True)
